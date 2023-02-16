27989d9cf15f319713a3e85dad6316c14be6524d
Connor Ferris <ferr0373@gmail.com>
2023-02-16 14:49:04 -0600

    clear button complete

--- src/components/ShoppingList/ShoppingList.jsx
+++ src/components/ShoppingList/ShoppingList.jsx
@@ -1,22 +1,30 @@
 import React from "react";
 import Item from "../Item/Item";
 
-function ShoppingList({shoppingList}) {
-  return (
-    <>
-      <h2>Shopping List</h2>
-      <div className="list-buttons">
-        <button>Reset</button>
-        <button>Clear</button>
-      </div>
-      <div className="list-grid">
+function ShoppingList({shoppingList, clearList}) {
 
-        {shoppingList.map(item => (
-        <Item key={item.id} item={item} />
-        ))}
-      </div>
-    </>
-  );
+
+    const handleClearClick = () => {
+        console.log('Cicked Clear');
+        clearList();
+    }
+
+
+    return (
+        <>
+            <h2>Shopping List</h2>
+                <div className="list-buttons">
+                    <button>Reset</button>
+                    <button onClick={handleClearClick} >Clear</button>
+                </div>
+            <div className="list-grid">
+
+            {shoppingList.map(item => (
+                <Item key={item.id} item={item} />
+            ))}
+            </div>
+        </>
+    );
 }
 
 export default ShoppingList;
