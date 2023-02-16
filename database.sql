a373a1a8160f2bdbf4bc31205e78cf340f725844
Connor Ferris <ferr0373@gmail.com>
2023-02-16 09:59:57 -0600

    project set-up

--- database.sql
+++ database.sql
@@ -1,2 +1,13 @@
 -- Don't forget to add your create table SQL 
 -- It is also helpful to include some test data
+
+CREATE TABLE "shopping_table" (
+    "id" SERIAL PRIMARY KEY,
+    "name" VARCHAR(80) NOT NULL,
+    "quantity" DECIMAL (4,2),
+    "unit" VARCHAR(20)
+);
+
+
+INSERT INTO "shopping_table" ("name", "quantity", "unit")
+VALUES ('applesauce', '5', 'jars');
