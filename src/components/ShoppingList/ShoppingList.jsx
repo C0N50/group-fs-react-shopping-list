2dbd3b7ab34c8b3cdb857ce414063d3fe4fbb2a3
Connor Ferris <ferr0373@gmail.com>
2023-02-16 11:43:42 -0600

    created shoppinglist framework

---
+++ src/components/ShoppingList/ShoppingList.jsx
@@ -0,0 +1,25 @@
+import React from "react"
+
+function ShoppingList () {
+
+    return (
+        <>
+        <h2>Shopping List</h2>
+        <div className = 'list-buttons'>
+            <button>Reset</button>
+            <button>Clear</button>
+        </div>
+        <div class = 'list-grid'>
+            
+            
+            {/* Add Item Component here */}
+
+
+        </div>
+        </>
+
+    )
+
+}
+
+export default ShoppingList;
