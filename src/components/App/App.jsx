79c7af0a2e2e6ede8225cae73b175b08b89e754f
Cyril Malle-Barlow <cyril.mallebarlow@gmail.com>
2023-02-16 12:06:56 -0600

    app.jsx has get and post, imports addform and shoppinglist

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,50 @@
 import React from 'react';
-
-import Header from '../Header/Header.jsx'
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import Header from '../Header/Header.jsx';
+import AddForm from '../AddForm/AddForm.jsx';
+import ShoppingList from '..ShoppingList/ShoppingList.jsx';
 import './App.css';
 
 
 function App() {
+
+    const [shoppingList, setShoppingList] = useState([]);
+
+    const getList = () => {
+
+        axios.get('/list')
+            .then((response) => {
+                console.log('in GET list', response);
+                setShoppingList(response.data);
+            }).catch((err) => {
+                console.error('GET error', err);
+            });
+    };
+
+    useEffect(() => {
+        getList();
+    }, []);
+
+    const addItem = (itemToAdd) => {
+
+        axios.post('/list', { itemToAdd })
+            .then((response) => {
+                console.log('in POST item', response);
+                getList();
+            }).catch((err) => {
+                console.error('in POST error', err);
+            });
+    };
+
+
+
     return (
         <div className="App">
             <Header />
             <main>
-                <p>Under Construction...</p>
+                <AddForm addItem={addItem} />
+                <ShoppingList shoppingList={shoppingList} />
             </main>
         </div>
     );
